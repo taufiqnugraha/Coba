1d05cae42ad511dce3d42c5c313347386fc640c6
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-10-01 20:10:36 +0700

    taufiq line 2 barus ke 4

--- coba.php
+++ coba.php
@@ -1,7 +1,3 @@
 <?php
 	echo "10114426 -Taufiq Nugraha";
-<<<<<<< HEAD
-	echo "10114474 - Faisal Sal sal sal";
-=======
-	echo "10114474 -Faisal Ishai";
->>>>>>> 33d1e2f93231ae8aa4c410c2ec9cbc9fc91158bc
+	echo "10114474 - Faisal gimana ini sal";
