16e1667b98f73e9e61e7c2c794a07d0a34bb4795
Angga Irawan <modhochi03@gmail.com>
2016-10-02 21:27:45 +0700

    tambahan nama+nim

--- coba.php
+++ coba.php
@@ -1,5 +1,6 @@
 <?php
 	echo "10114426 -Taufiq Nugraha";
 	echo "10114474 -Rizal <3 Fitri";
-	echo "10114453 - Mike Aria Dwiputri Santoso"
+	echo "10114453 - Mike Aria Dwiputri Santoso";
+	echo "10114131 -Angga Irawan";
 
