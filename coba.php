315e28ad43fd9415c1f8e68b94caf28b9280a8ec
taufiqnugraha <taufiqnugraha48@gmail.com>
2016-10-09 20:11:02 +0700

    Oke udah

--- coba.php
+++ coba.php
@@ -1,11 +1,5 @@
 <?php
 	echo "10114426 - Taufiq Nugraha";	
-	echo "10114426 -Taufiq Nugraha";	
-	echo "10114453 - Mike Aria Dwiputri Santoso";
-	echo "10114426 -Taufiq Nugraha";
-	echo "10114341 -Fitri Febriana";
-	echo "10114453 - Mike Aria Dwiputri Santoso";
-	echo "10114131 -Angga Irawan";
-	echo "10114474 - Faisal Ishak";
+
 
 
